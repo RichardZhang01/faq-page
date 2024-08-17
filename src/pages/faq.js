43fb70497ef1c17e6e6d873617c4eeac72859eab
Richard Zhang <richardzhiyuanzhang@gmail.com>
2024-08-17 17:16:36 -0400

    add expand/collapse all button

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+
 const faqs = [
   {
     question: "What is Next.js?",
@@ -15,15 +16,14 @@ const faqs = [
   },
 ];
 
-const Accordion = ({ question, answer }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const Accordion = ({ question, answer, isOpen, toggleAccordion }) => {
   const contentRef = useRef(null);
 
   return (
     <div className="rounded-lg mb-4">
       <button
         className="w-full p-4 sm:p-5 md:p-6 text-left bg-black hover:bg-gray-900 border-t border-cyan-400"
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={toggleAccordion}
       >
         <div className="flex justify-between items-center">
           <span className="font-semibold text-sm sm:text-base md:text-lg">
@@ -69,14 +69,29 @@ const SearchBar = ({ searchTerm, handleSearch }) => (
       placeholder="Search for a Question"
       value={searchTerm}
       onChange={handleSearch}
-      className="w-full p-2 sm:p-3 md:p-4 rounded-xl border border-cyan-400 bg-black text-white placeholder-gray-400 focus:outline-none focus:border-white"
+      className="w-full p-2 sm:p-3 md:p-4 rounded-xl border border-cyan-400 bg-black hover:bg-gray-900 text-white placeholder-gray-400 focus:outline-none focus:border-white"
     />
+  </div>
+);
+
+const ExpandCollapseAllButton = ({
+  areAllExpanded,
+  handleExpandCollapseAll,
+}) => (
+  <div className="flex justify-center md:justify-end mb-6">
+    <button
+      onClick={handleExpandCollapseAll}
+      className="w-full md:w-32 text-sm sm:text-base md:text-lg px-4 py-2 mt-2 bg-white hover:bg-cyan-400 text-black hover:text-white font-semibold rounded-lg transition-all duration-300"
+    >
+      {areAllExpanded ? "Collapse All" : "Expand All"}
+    </button>
   </div>
 );
 
 const FAQ = () => {
   const [filteredFaqs, setFilteredFaqs] = useState(faqs);
   const [searchTerm, setSearchTerm] = useState("");
+  const [expandedFaqs, setExpandedFaqs] = useState([]);
 
   const handleSearch = (e) => {
     const searchValue = e.target.value.toLowerCase();
@@ -86,6 +101,21 @@ const FAQ = () => {
       faq.question.toLowerCase().includes(searchValue)
     );
     setFilteredFaqs(filtered);
+    setExpandedFaqs([]); // Collapse all on new search
+  };
+
+  const toggleAccordion = (index) => {
+    setExpandedFaqs((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
+    );
+  };
+
+  const handleExpandCollapseAll = () => {
+    if (expandedFaqs.length === filteredFaqs.length) {
+      setExpandedFaqs([]);
+    } else {
+      setExpandedFaqs(filteredFaqs.map((_, index) => index));
+    }
   };
 
   return (
@@ -99,9 +129,20 @@ const FAQ = () => {
       <SearchBar searchTerm={searchTerm} handleSearch={handleSearch} />
 
       <div className="max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl mx-auto">
+        <ExpandCollapseAllButton
+          areAllExpanded={expandedFaqs.length === filteredFaqs.length}
+          handleExpandCollapseAll={handleExpandCollapseAll}
+        />
+
         {filteredFaqs.length > 0 ? (
           filteredFaqs.map(({ question, answer }, i) => (
-            <Accordion question={question} answer={answer} key={i} />
+            <Accordion
+              key={i}
+              question={question}
+              answer={answer}
+              isOpen={expandedFaqs.includes(i)}
+              toggleAccordion={() => toggleAccordion(i)}
+            />
           ))
         ) : (
           <p className="text-center text-white">
