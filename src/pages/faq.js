11eb40b03b21bb4bcf7a572cbb5197c12a5e4066
Richard Zhang <richardzhiyuanzhang@gmail.com>
2024-08-30 11:44:16 -0400

    add comments to faq page

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
 
+// hard coded array of faq objects
 const faqs = [
   {
     question: "What is Next.js?",
@@ -17,11 +18,15 @@ const faqs = [
   },
 ];
 
+// Accordion component for each faq object
 const Accordion = ({ question, answer, isOpen, toggleAccordion }) => {
+  // contentRef used to set height of the answer portion of the faq
+  // mainly used for a smooth animation transition between open and closed state
   const contentRef = useRef(null);
 
   return (
     <div className="rounded-lg mb-4">
+      {/* Question portion of the faq */}
       <button
         className="w-full p-4 sm:p-5 md:p-6 text-left hover:bg-gray-900 border-t border-teal-600"
         onClick={toggleAccordion}
@@ -30,6 +35,7 @@ const Accordion = ({ question, answer, isOpen, toggleAccordion }) => {
           <span className="font-semibold text-sm sm:text-base md:text-lg">
             {question}
           </span>
+          {/* A caret that rotates when expanding and retracting the accordion */}
           <svg
             className={`w-5 h-5 sm:w-6 sm:h-6 transform transition-transform duration-300 ${
               isOpen ? "rotate-180" : ""
@@ -48,6 +54,7 @@ const Accordion = ({ question, answer, isOpen, toggleAccordion }) => {
           </svg>
         </div>
       </button>
+      {/* Answer portion of the faq */}
       <div
         ref={contentRef}
         className="overflow-hidden transition-all duration-300 ease-in-out"
@@ -63,11 +70,12 @@ const Accordion = ({ question, answer, isOpen, toggleAccordion }) => {
   );
 };
 
+// Search bar component that runs handleSearch to filter the faq array
 const SearchBar = ({ searchTerm, handleSearch }) => (
   <div className="mb-6 sm:mb-8 max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl mx-auto">
     <input
       type="text"
-      placeholder="Search for a Question"
+      placeholder="Search for a question"
       value={searchTerm}
       onChange={handleSearch}
       className="w-full p-2 sm:p-3 md:p-4 rounded-xl border border-teal-600 bg-black hover:bg-gray-900 text-white placeholder-gray-400 focus:outline-none focus:border-white"
@@ -75,6 +83,7 @@ const SearchBar = ({ searchTerm, handleSearch }) => (
   </div>
 );
 
+// button to set all accordion components to open/closed
 const ExpandCollapseAllButton = ({
   areAllExpanded,
   handleExpandCollapseAll,
@@ -89,12 +98,14 @@ const ExpandCollapseAllButton = ({
   </div>
 );
 
+// The FAQ component itself containing all above components
 const FAQ = () => {
   const [filteredFaqs, setFilteredFaqs] = useState(faqs);
   const [searchTerm, setSearchTerm] = useState("");
   const [expandedFaqs, setExpandedFaqs] = useState([]);
   const router = useRouter();
 
+  // filters on component load by whatever search term was input into the search bar previously
   useEffect(() => {
     const querySearchTerm = router.query.search || "";
     setSearchTerm(querySearchTerm);
@@ -105,6 +116,7 @@ const FAQ = () => {
     setFilteredFaqs(filtered);
   }, [router.query.search]);
 
+  // takes the search term from the search bar and filters the faq objects in the faq array by the search term
   const handleSearch = (e) => {
     const searchValue = e.target.value.toLowerCase();
     setSearchTerm(searchValue);
@@ -118,12 +130,14 @@ const FAQ = () => {
     router.push(`?search=${searchValue}`, undefined, { shallow: true });
   };
 
+  // handles logic related to open/closed state of each individual accordion
   const toggleAccordion = (index) => {
     setExpandedFaqs((prev) =>
       prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
     );
   };
 
+  // sets all accordion components to open/closed
   const handleExpandCollapseAll = () => {
     if (expandedFaqs.length === filteredFaqs.length) {
       setExpandedFaqs([]);
@@ -134,18 +148,22 @@ const FAQ = () => {
 
   return (
     <div className="p-4 sm:p-6 md:p-8 ">
+      {/* main header */}
       <h1 className="mb-6 sm:mb-8 font-bold text-2xl sm:text-3xl md:text-4xl text-center">
         Frequently Asked Questions
       </h1>
 
+      {/* search bar */}
       <SearchBar searchTerm={searchTerm} handleSearch={handleSearch} />
 
+      {/* collapse/expand all button */}
       <div className="max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl mx-auto">
         <ExpandCollapseAllButton
           areAllExpanded={expandedFaqs.length === filteredFaqs.length}
           handleExpandCollapseAll={handleExpandCollapseAll}
         />
 
+        {/* Accordion component for each faq object */}
         {filteredFaqs.length > 0 ? (
           filteredFaqs.map(({ question, answer }, i) => (
             <Accordion
@@ -157,6 +175,7 @@ const FAQ = () => {
             />
           ))
         ) : (
+          // text displayed when no questions contain the search query
           <p className="text-center text-white">
             No questions found matching your query
           </p>
